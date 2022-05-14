c65f3382288ade195133d10f208eaf8cd72001fe
rnstphns <97855804+rnstphns@users.noreply.github.com>
2022-05-14 13:43:14 -0500

    Add files via upload

---
+++ xkcd936generator/dictionary/src/gui/xkcdMainFrame.java
@@ -0,0 +1,43 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class xkcdMainFrame {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					xkcdMainFrame window = new xkcdMainFrame();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public xkcdMainFrame() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ xkcd936generator/dictionary/src/rng/Test.java
@@ -0,0 +1,15 @@
+package rng;
+
+public class Test {
+
+	public static void main(String[] args) {
+		RNG r = new RNG();
+		System.out.println(r.randomWord());
+		System.out.println(r.randomWord());
+		System.out.println(r.randomWord());
+		System.out.println(r.randomWord());
+		System.out.println(r.randomWord());
+
+	}
+
+}
