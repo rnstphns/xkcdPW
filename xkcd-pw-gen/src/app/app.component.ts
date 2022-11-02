61c3f6d79609f2ffb646d266fe171a09acc2cfc5
rnstphns <97855804+rnstphns@users.noreply.github.com>
2022-11-02 16:38:18 -0500

    style index page

--- xkcd-pw-gen/src/app/app.component.ts
+++ xkcd-pw-gen/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<p>root works</p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'xkcd-pw-gen';
+
+  
 }
