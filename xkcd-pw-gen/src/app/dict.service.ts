ff7bb39cc21843bf2c578732c25163c4b787c6f8
rnstphns <97855804+rnstphns@users.noreply.github.com>
2022-11-22 22:02:00 -0600

    setup for aws deployment

--- xkcd-pw-gen/src/app/dict.service.ts
+++ xkcd-pw-gen/src/app/dict.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {  Injectable } from '@angular/core'
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DictService {
 
-  url: string = 'http://localhost:3000/'
+  url: string = environment.backendUrl;
   
   constructor(private http: HttpClient) { }
 
   generatePass(form: JSON): Observable<Object> {
+    // console.log(`recieved req ${JSON.stringify(form)}`)
     return this.http.post(this.url, form,
       { headers: new HttpHeaders({ "Content-Type": "application/json" }) }
     )
--- xkcd-pw-gen/src/environments/environment.prod.ts
+++ xkcd-pw-gen/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  backendUrl: 'Passwordbackend-env.eba-mzjpfxpb.us-east-1.elasticbeanstalk.com '
 };
